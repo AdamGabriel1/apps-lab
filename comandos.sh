8b7a1369f176dc2e1d44cf14cca213c023ad7f21
Adam Gabriel <adamgabriel289@gmail.com>
2025-09-15 15:11:40 -0400

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install wget -y
-wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" --no-check-certificate
 sudo apt install ./code_1.104.0-1757488003_amd64.deb - y
 wget https://download-cdn.jetbrains.com/python/pycharm-2025.2.1.1.tar.gz
 sudo tar xzf pycharm-*.tar.gz -C /opt/
