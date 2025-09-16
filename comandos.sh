e985f582bec0a514f1fb024d7b54cc97efb74644
Adam Gabriel <adamgabriel289@gmail.com>
2025-09-16 13:37:58 -0400

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install neofetch -y
 sudo apt install wget -y
 wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/f220831ea2d946c0dcb0f3eaa480eb435a2c1260/code_1.104.0-1757488003_amd64.deb
 sudo apt install ./code_1.104.0-1757488003_amd64.deb -y
